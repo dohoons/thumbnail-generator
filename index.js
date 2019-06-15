dd7fd4304ac31b08a80374c24f1f72dccc4353dd
dohoons <dohoons@gmail.com>
2019-06-15 16:29:13 +0900

    코드개선, 오류수정

--- index.js
+++ index.js
@@ -7,36 +7,48 @@ const figlet = require('figlet');
 const loadIniFile = require('read-ini-file');
 const package = require('./package.json');
 
-const configFileName = 'thumbnail-generate-config.ini';
+const configFileName = 'thumbnail-config.ini';
 
-function generate(directory) {
-  let config = {};
+function loadConfig(directory) {
+  const dirname = directory ? directory : __dirname;
+  const configPath = path.join(dirname, configFileName);
 
   try {
-    const configPath = path.join(directory ? directory : __dirname, configFileName);
-    config = loadIniFile.sync(configPath);
-  } catch (e) {
+    const config = loadIniFile.sync(configPath);
+    const { outputName, outputFormat } = config;
+    const input = path.join(dirname, config.input);
+    const outputDir = path.join(dirname, config.outputDir);
+    const outputSizeSet = [...config.thumnail.map(set => JSON.parse(set))];
+
+    return {
+      input,
+      outputDir,
+      outputName,
+      outputFormat,
+      outputSizeSet,
+    };
+
+  } catch(e) {
     console.clear();
-    console.log(path.join(directory ? directory : __dirname, configFileName));
+    console.log(e)
     console.log(chalk.rgb(255, 0, 0)(`=== ERROR : 설정파일을 불러오는데 실패했습니다.`));
+    console.log(configPath);
+    if(e instanceof SyntaxError) {
+      console.log(chalk.rgb(255, 0, 0)('    설정파일 형식이 잘못되었습니다.'))
+    }
     start();
-    return;
   }
+}
 
-  const { outputName, outputFormat } = config;
-  const outputSizeSet = [...config.thumnail.map(set => JSON.parse(set))];
-  const works = [];
-
-  const input = path.join(directory ? directory : __dirname, config.input);
-  const outputDir = path.join(directory ? directory : __dirname, config.outputDir);
-  // console.log(input);
-  // console.log(outputDir);
-  
+function generate(directory) {
+  const { input, outputDir, outputName, outputFormat, outputSizeSet } = loadConfig(directory);
+    
   if(fs.existsSync(outputDir) === false) {
     fs.mkdirSync(outputDir);
   }
 
-  outputSizeSet.forEach(({ width, height, background, fit, quality, progressive }) => {
+  const works = outputSizeSet.map(({ width, height, background, fit, quality, progressive }) => {
+    const outputPath = path.join(outputDir, `${outputName}-${width ? width : 'auto'}x${height ? height : 'auto'}.${outputFormat}`);
     let work = sharp(input);
 
     if(outputFormat === 'jpg') {
@@ -47,72 +59,75 @@ function generate(directory) {
     }
 
     work.resize(width, height, {
-      fit: fit ? fit : 'contain',
-      background: background ? background : outputFormat === 'png' ? { r: 255, g: 255, b: 255, alpha: 0 } : '#ffffff'
-    }).toFile(path.join(outputDir, `${outputName}-${width}x${height}.${outputFormat}`));
+      fit: fit || 'contain',
+      background: background ? `#${background}` : outputFormat === 'png' ? { r: 255, g: 255, b: 255, alpha: 0 } : '#ffffff'
+    }).toFile(outputPath);
       
-    works.push(work);
+    return work;
   });
 
-  Promise.all(works).finally(() => {
-    console.log(chalk.rgb(0, 255, 255)(`=== 썸네일 생성 결과 ===
-${outputSizeSet.map(({ width, height }) => {
-  return path.join(outputDir, `${outputName}-${width}x${height}.${outputFormat}`);
-}).join('\n')}
-`));
-
+  Promise.all(works).then(res => {
+    console.log(chalk.rgb(0, 255, 255)(`=== 썸네일 생성 결과 ===\n  ${res.map(r => r.options.fileOut).join('\n  ')}\n`));
     exit();
   });
 }
 
 function start() {
   figlet('Thumbnail Generator', function(err, data) {
-      if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
+    if(err) {
+      console.log('Something went wrong...');
+      console.dir(err);
 
-        setTimeout(() => {}, 50000)
-        return;
-      }
+      setTimeout(() => {}, 50000)
+      return;
+    }
 
-      console.log(data);
-      console.log(`   author: ${package.author.name}(${package.author.email}), version: v${package.version}\n`);
+    console.log(data);
+    console.log(`   author: ${package.author.name}(${package.author.email}), version: v${package.version}\n`);
 
-      inquirer.prompt([{
+    inquirer.prompt([
+      {
         type: 'input',
         name: 'directory',
-        message: '이미지 설정 파일이 위치한 폴더의 경로를 입력하세요.',
-      }, {
+        message: '썸네일 설정 파일 경로를 입력하세요. 예) C:\\mywork\n마우스 우클릭으로 폴더경로 붙여넣기 후 엔터 =>',
+      },
+      {
         type: 'list',
         name: 'confirm',
-        choices: ['예', '아니오'],
+        choices: ['예', '아니오', '다시시작'],
         message: '썸네일 이미지를 생성하시겠습니까?',
-      }])
-        .then((answers) => {
-          if(answers.confirm === '예') {
-            console.log(chalk.rgb(255, 255, 0)(`=== 대상경로 : ${answers.directory ? answers.directory : '.'}/${configFileName}`));
-            generate(answers.directory);
-          } else {
-            exit();
-          }
-        });
+      },
+    ])
+      .then((answers) => {
+        if(answers.confirm === '예') {
+          console.log(chalk.rgb(255, 255, 0)(`=== 대상경로===\n  ${path.join(answers.directory ? answers.directory : __dirname, configFileName)}`));
+          generate(answers.directory);
+        } else if(answers.confirm === '다시시작') {
+          console.clear();
+          start();
+        } else {
+          exit();
+        }
+      });
   });
 }
 
 function exit() {
-  inquirer.prompt([{
-    type: 'list',
-    name: 'exit',
-    choices: ['종료', '처음부터'],
-    message: '프로그램을 종료하시겠습니까?',
-  }])
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'exit',
+      choices: ['종료', '처음부터'],
+      message: '프로그램을 종료하시겠습니까?',
+    },
+  ])
     .then((answers) => {
       if(answers.exit === '처음부터') {
         console.clear();
         start();
       } else {
         console.log('EXIT');
-        setTimeout(() => {}, 500);
+        setTimeout(() => {}, 300);
       }
     });
 }
